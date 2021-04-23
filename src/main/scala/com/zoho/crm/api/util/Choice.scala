3e6563ba992b63ba679208179e3250c64e1b93f1
pravesh.a <pravesh.a@zohocorp.com>
2021-04-23 20:40:18 +0530

    0.0.2

---
+++ src/main/scala/com/zoho/crm/api/util/Choice.scala
@@ -0,0 +1,15 @@
+package com.zoho.crm.api.util
+
+
+class Choice[T]{
+  var value:T =_
+
+  def this(value: T) {
+    this()
+    this.value = value.asInstanceOf[T]
+  }
+
+  def getValue: T ={
+    this.value
+  }
+}
